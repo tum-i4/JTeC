da9d3914fd7462e5945439cc5ee96691c0c793cd
Daniel Elsner <daniel.elsner@tum.de>
2022-06-07 11:32:49 +0200

    Move pre-test hook to agent class

--- jtec-agent/src/main/java/edu/tum/sse/jtec/agent/JTeCAgent.java
+++ jtec-agent/src/main/java/edu/tum/sse/jtec/agent/JTeCAgent.java
@@ -1,14 +1,29 @@
 package edu.tum.sse.jtec.agent;
 
+import edu.tum.sse.jtec.util.ProcessUtils;
+
 import java.lang.instrument.Instrumentation;
+import java.util.Collections;
 
 public class JTeCAgent {
+
+    public static final String PID_KEY = "JTEC_PID";
+
     public static void premain(String arguments, Instrumentation instrumentation) {
         if (arguments == null) {
             throw new RuntimeException("Missing arguments with log file path for instrumentation.");
         }
         AgentOptions options = AgentOptions.fromString(arguments);
         System.err.println("Attaching JTeC agent with args: " + options.toAgentString());
+
+        // Before we start tracing, we need to run the pre-test command, if specified.
+        if (!options.getPreTestCommand().isEmpty()) {
+            try {
+                ProcessUtils.run(options.getPreTestCommand(), Collections.singletonMap(PID_KEY, ProcessUtils.getCurrentPid()), false);
+            } catch (Exception e) {
+                System.err.println("Failed to run pre-test command " + options.getPreTestCommand() + " : " + e.getMessage());
+            }
+        }
 
         new Tracer(instrumentation, options);
     }
