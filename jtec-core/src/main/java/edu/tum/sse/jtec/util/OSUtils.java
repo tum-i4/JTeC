5fd194823de8748c2784dc9265e10f082968a061
Daniel Elsner <daniel.elsner@tum.de>
2022-06-29 09:21:10 +0200

    [core] fix os utils test

--- jtec-core/src/main/java/edu/tum/sse/jtec/util/OSUtils.java
+++ jtec-core/src/main/java/edu/tum/sse/jtec/util/OSUtils.java
@@ -8,7 +8,7 @@ public final class OSUtils {
 
     public static String wrapEnvironmentVariable(String command, String envVar) {
         if (isWindows()) {
-            return command.replaceAll("[^%](" + envVar + ")", "%" + envVar + "%");
+            return command.replaceAll("([^%])(" + envVar + ")", "$1%$2%");
         } else {
             return command.replaceAll("([^${])(" + envVar + ")", "$1\\$\\{$2\\}");
         }
