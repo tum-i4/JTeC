7d5317d1a7bd1b32c08c7bbdd38cdd1f9f26332e
Raphael Nömmer <noemmer@cqse.eu>
2022-10-18 10:37:28 +0200

    [maven-plugin] Change autoinclude to use class dir instead of source

--- jtec-maven-plugin/src/main/java/edu/tum/sse/jtec/mojo/JTeCMojo.java
+++ jtec-maven-plugin/src/main/java/edu/tum/sse/jtec/mojo/JTeCMojo.java
@@ -8,13 +8,16 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import static edu.tum.sse.jtec.util.IOUtils.locateJar;
 
@@ -25,57 +28,58 @@ import static edu.tum.sse.jtec.util.IOUtils.locateJar;
  * <a href="https://maven.apache.org/surefire/maven-surefire-plugin/test-mojo.html#debugForkedProcess">Surefire</a> or
  * <a href="https://maven.apache.org/surefire/maven-failsafe-plugin/integration-test-mojo.html#debugForkedProcess">Failsafe</a> debug option.
  */
-@Mojo(name = "jtec", defaultPhase = LifecyclePhase.INITIALIZE, threadSafe = true)
+@Mojo(name = "jtec", defaultPhase = LifecyclePhase.PROCESS_TEST_CLASSES, threadSafe = true)
 public class JTeCMojo extends AbstractJTeCMojo {
 
+    public static final char PACKAGE_SEPARATOR = '.';
     private static final String SUREFIRE_DEBUG_OPTION = "maven.surefire.debug";
     private static final String FAILSAFE_DEBUG_OPTION = "maven.failsafe.debug";
     private static final String META_INF = "META-INF";
-
-
     /**
      * JTeC options passed to the JTeC agent.
      */
     @Parameter(property = "jtec.opts", readonly = true)
     String agentOpts;
 
-    @Parameter(property = "jtec.autoinclude", readonly = true)
+    /**
+     * JTeC option to generate include patterns automatically.
+     * Only works reliably if the test and source classes are in the same module.
+     */
+    @Parameter(property = "jtec.autoinclude", readonly = true, defaultValue = "false")
     Boolean autoinclude;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
             log("Executing JTeC Maven plugin with agentOpts=" + agentOpts + " for project " + project.getName());
-            String preparedAgentOpts = prepareAgentOpts();
-            Path agentJar = locateJar(JTeCAgent.class);
-            Properties properties = project.getProperties();
-            for (String property : new String[]{SUREFIRE_DEBUG_OPTION, FAILSAFE_DEBUG_OPTION}) {
-                String oldValue = properties.getProperty(property);
-                String newValue = String.format("%s-javaagent:%s=%s", (oldValue == null ? "" : oldValue + " "), agentJar.toAbsolutePath(), preparedAgentOpts);
+            final String preparedAgentOpts = prepareAgentOpts();
+            final Path agentJar = locateJar(JTeCAgent.class);
+            final Properties properties = project.getProperties();
+            for (final String property : new String[]{SUREFIRE_DEBUG_OPTION, FAILSAFE_DEBUG_OPTION}) {
+                final String oldValue = properties.getProperty(property);
+                final String newValue = String.format("%s-javaagent:%s=%s", (oldValue == null ? "" : oldValue + " "), agentJar.toAbsolutePath(), preparedAgentOpts);
                 properties.setProperty(property, newValue);
                 log(String.format("Changing Maven property %s to %s.", property, newValue));
             }
-        } catch (Exception exception) {
+        } catch (final Exception exception) {
             getLog().error("Failed to find JTeC agent JAR, skipping instrumentation.");
             exception.printStackTrace();
         }
     }
 
     private String prepareAgentOpts() {
-        AgentOptions agentOptions;
+        final AgentOptions agentOptions;
         if (agentOpts == null) {
             agentOptions = AgentOptions.DEFAULT_OPTIONS;
         } else {
             agentOptions = AgentOptions.fromString(agentOpts);
         }
         agentOptions.setOutputPath(outputDirectory.toPath());
-        if (autoinclude) {
+        if (autoinclude && Files.exists(Paths.get(project.getBuild().getOutputDirectory()))) {
             String autoincludePatterns = "";
             try {
-                String sourceDirectory = project.getBuild().getSourceDirectory();
-                autoincludePatterns = findAutoincludePatterns(Paths.get(sourceDirectory), sourceDirectory);
-                autoincludePatterns = "(" + autoincludePatterns + ")" + ".*";
-            } catch (IOException e) {
+                autoincludePatterns = buildAutoIncludePattern();
+            } catch (final IOException e) {
                 getLog().warn("No Source dir found!", e);
             }
             if (!autoincludePatterns.isEmpty()) {
@@ -88,24 +92,37 @@ public class JTeCMojo extends AbstractJTeCMojo {
         return agentString;
     }
 
-    private String findAutoincludePatterns(Path path, String remainingPath) throws IOException {
-        List<Path> paths = new ArrayList<>();
-        log("Searching for autoinclude Patterns in " + path);
-        Files.list(path).forEach(filePath -> {
-            if (!filePath.getFileName().endsWith(META_INF)) {
-                paths.add(filePath);
-            }
-        });
-        if (paths.stream().anyMatch(currentPath -> !Files.isDirectory(currentPath))) {
-            return path.toString().substring(remainingPath.length() + 1);
-        }
-        StringBuilder result = new StringBuilder();
-        for (Path currentPath : paths) {
-            result.append(findAutoincludePatterns(currentPath, remainingPath));
-            result.append(';');
+    private String buildAutoIncludePattern() throws IOException {
+        final Set<String> includePaths = new HashSet<>();
+        final String classDirectory = project.getBuild().getOutputDirectory();
+        findAutoincludePatterns(Paths.get(classDirectory), classDirectory, includePaths);
+
+        final String testClassDir = project.getBuild().getTestOutputDirectory();
+        findAutoincludePatterns(Paths.get(testClassDir), testClassDir, includePaths);
+
+        final StringBuilder result = new StringBuilder();
+        for (final String path : includePaths) {
+            result.append(path);
+            result.append(AgentOptions.PIPE_REPLACEMENT);
         }
         result.deleteCharAt(result.length() - 1);
-        return result.toString().replace('/', '.');
+        final String includePackages = result.toString().replace(File.separatorChar, PACKAGE_SEPARATOR);
+        return "(" + includePackages + ")" + ".*";
+    }
+
+    private void findAutoincludePatterns(final Path path, final String classDir, final Set<String> includePaths) throws IOException {
+        final List<Path> paths = new ArrayList<>();
+        log("Searching for autoinclude Patterns in " + path);
+        Files.list(path).forEach(paths::add);
+        if (paths.stream().anyMatch(currentPath -> currentPath.toString().endsWith(".class"))) {
+            includePaths.add(path.toString().substring(classDir.length() + 1));
+            return;
+        }
+        for (final Path currentPath : paths) {
+            if (Files.isDirectory(currentPath)) {
+                findAutoincludePatterns(currentPath, classDir, includePaths);
+            }
+        }
     }
 
 }
